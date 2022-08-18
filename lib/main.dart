b6db912dd761ebf61d013e7044f283b0f9b190a0
powskierman <mlapoint99@hotmail.com>
2022-08-18 17:19:12 -0400

    Neumorphic switches for doors work.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: avoid_print
 
-import 'package:flutter/material.dart';
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:remote/mqtt_ctl.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 String appTitle = 'Ouvre Porte Garage';
 Color trackColor = Colors.green;
@@ -39,17 +39,60 @@ class _RootPageState extends State<RootPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // floatingActionButton:NeumorphicFloatingActionButton(
+      // child: const Icon(Icons.garage_outlined, size: 190, color: Colors.green),
+      // onPressed: (() {
+      //   print("onClick");},
+      //),
+      backgroundColor: NeumorphicTheme.baseColor(context),
       appBar: AppBar(
         title: Text(appTitle),
       ),
+
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: const [
-              Icon(Icons.garage_outlined, size: 190, color: Colors.green),
-              Icon(Icons.garage_outlined, size: 190, color: Colors.green),
+            children: [
+              NeumorphicButton(
+                onPressed: () {
+                  print("leftDoor");
+                },
+                style: NeumorphicStyle(
+                  shape: NeumorphicShape.flat,
+                  depth: 20,
+                  boxShape:
+                      NeumorphicBoxShape.roundRect(BorderRadius.circular(12)),
+                ),
+                padding: const EdgeInsets.all(12.0),
+                child: const Icon(Icons.garage_outlined,
+                    size: 190, color: Colors.green),
+                // Icon(
+                //   Icons.favorite_border,
+                //   //color: _iconsColor(context),
+                // ),
+              ),
+              NeumorphicButton(
+                onPressed: () {
+                  print("rightDoor");
+                },
+                style: NeumorphicStyle(
+                  shape: NeumorphicShape.flat,
+                  depth: 20,
+                  boxShape:
+                      NeumorphicBoxShape.roundRect(BorderRadius.circular(12)),
+                ),
+                padding: const EdgeInsets.all(12.0),
+                child: const Icon(Icons.garage_outlined,
+                    size: 190, color: Colors.green),
+                // Icon(
+                //   Icons.favorite_border,
+                //   //color: _iconsColor(context),
+                // ),
+              ),
+              //Icon(Icons.garage_outlined, size: 190, color: Colors.green),
+              // Icon(Icons.garage_outlined, size: 190, color: Colors.green),
             ],
           ),
           const SwitchScreen(),
